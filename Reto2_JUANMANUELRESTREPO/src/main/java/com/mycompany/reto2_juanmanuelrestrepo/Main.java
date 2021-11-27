d7866b1e78a053c77d558306d6b151a508f42a82
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2021-11-27 16:42:55 -0500

    Version_1
    Codigo con el ejercicio 1 ya realizado

--- Reto2_JUANMANUELRESTREPO/src/main/java/com/mycompany/reto2_juanmanuelrestrepo/Main.java
+++ Reto2_JUANMANUELRESTREPO/src/main/java/com/mycompany/reto2_juanmanuelrestrepo/Main.java
@@ -9,28 +9,63 @@ public class Main {
         while(ejecucion==1){
             System.out.println("Seleccione un ejercicio");
         System.out.println("opciones: 1) 2) 3)");
-        Scanner opcion = new Scanner(System.in);
-        String opcion_aux = opcion.next();
-        int opcion_final = Integer.parseInt(opcion_aux);
-        if(opcion_final==1){
-            System.out.println("Ejercicio 1");
-            
-        }
-        if(opcion_final==2){
+        Scanner lector_input = new Scanner(System.in);
+        if(lector_input.hasNextInt()==true){
+            int opcion = lector_input.nextInt();
+            if(opcion==1){
+               ejercicio_1();   
+            }
+            if(opcion==2){
             System.out.println("ejercicio 2");
-        }
-        if(opcion_final==3){
+            }
+            if(opcion==3){
             System.out.println("ejercicio 3");
+             }
         }
+        else{
+            System.out.println("Por favor introduzca una opción valida");
+        }   
+        
         System.out.println("¿Desea continuar la ejecucion?");
         System.out.println("Si(1) No(0)");
-        Scanner ejecucion_aux = new Scanner(System.in);
-        String ejecucion2_aux = ejecucion_aux.next();
-        ejecucion = Integer.parseInt(ejecucion2_aux);
-        if(ejecucion!=1)
-        {
-            System.out.println("Adios");
+        lector_input = new Scanner(System.in);
+        if(lector_input.hasNextInt()==true){
+            ejecucion = lector_input.nextInt();
+            if(ejecucion==0)
+            {
+                System.out.println("Adios");
+            }
+        }
+        else{
+            System.out.println("Por favor digite una opción valida");
         }
         }     
+    }
+    static void ejercicio_1()
+    {
+        System.out.println("Ingrese el numero n");
+        Scanner input_numeros = new Scanner(System.in);
+        if(input_numeros.hasNextInt()==true)
+        {
+            System.out.println("Ingrese el numero m");
+            int numero_n = input_numeros.nextInt();
+            input_numeros = new Scanner(System.in);
+            if(input_numeros.hasNextInt()==true)
+            {
+                int numero_m = input_numeros.nextInt();
+                for(int i=1; i<=numero_n; i++)
+                {
+                    for(int j=1; j<=numero_m; j++)
+                    {
+                        System.out.print(i*j+" ");
+                    }
+                    System.out.println("");
+                }
+            }
+        }
+    }
+    static void ejercicio_2()
+    {
+        
     }
 }
